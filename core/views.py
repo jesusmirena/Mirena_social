be5ab64077b623379eefe0b871627ec012acbe56
jesusmirena <jesusamh16@gmail.com>
2022-06-29 01:26:39 -0500

    Signin view and template

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 from core.models import Profile
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -43,3 +45,20 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
